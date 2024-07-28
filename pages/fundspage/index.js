2fd3a24b18c718d3d8eca7bf4288859bbf6b4adb
Minfei Jin <mjin@pdsit.net>
2024-07-28 16:29:45 +0800

    baike

--- pages/fundspage/index.js
+++ pages/fundspage/index.js
@@ -7,4 +7,14 @@ Page({
   },
   onLoad() {
   },
+  toBaike() {
+    console.log('to baidu bake');
+    wx.navigateToMiniProgram({
+      shortLink: '#小程序://搜狗百科/S7zgdnhtlJCKDoI',
+      success(res){
+        console.log('navigate success: ' + res);
+      }
+    });
+
+  }
 })
